59eaba2c0020f5d40dfb83078c3ed3f04a0e8a37
baudelotphilippe <philippe@baudelot.eu>
2023-01-23 14:51:19 +0100

    add contact

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -2,10 +2,16 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use App\Form\ContactType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class VinylController extends AbstractController
 {
@@ -13,5 +19,22 @@ class VinylController extends AbstractController
     public function index(): Response
     {
         return $this->render("base.html.twig", ['h1' =>"titre de la page"]);
+    }
+
+    #[Route('/contact', name:'contact')]
+    public function contactPage(Request $request): Response
+    {
+        $form = $this->createForm(ContactType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // data is an array with "name", "email", and "message" keys
+            $data = $form->getData();
+            dump($data);
+        }
+
+        return $this->render('projet/contact.html.twig', [
+            'form' => $form,
+        ]);
     }
 }
